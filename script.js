8a81b567be2f4711e5af76d949c44c3cf3cd6bb9
Sayantan Maity <sayantan.18maity@gmail.com>
2026-02-06 20:17:37 +0530

    Added edit feature to todo app

--- script.js
+++ script.js
@@ -4,15 +4,28 @@ const tasks = document.querySelector(".tasks")
 const noTask = document.querySelector(".noTask")
 
 addBtn.addEventListener("click", () => {
+    
+    if (taskInp.value.trim() === ""){
+        alert("please write something first")
+        return
+    }
+
     let task = document.createElement("div")
     task.classList.add("task")
     task.innerHTML = `
-                        <h3>${taskInp.value}</h3>
+                        <h3 class="textEl">${taskInp.value}</h3>
                         <button class="deleteBtn">
                         <i class="ri-delete-bin-6-line"></i>
                         </button>
+                        <button class="editBtn">
+                        <i class="ri-edit-2-line"></i>
+                        </button>
                     `
-
+    let textEl = task.querySelector(".textEl")
+    textEl.addEventListener("dblclick", () =>{
+        textEl.style.textDecoration = "line-through";
+    })
+    
     let deleteBtn = task.querySelector(".deleteBtn")
     deleteBtn.addEventListener("click", () => {
         task.remove()
@@ -20,6 +33,12 @@ addBtn.addEventListener("click", () => {
         if (document.querySelectorAll(".task").length === 0) {
             noTask.style.display = "block"
         }
+    })
+
+    let editBtn = task.querySelector(".editBtn")
+    editBtn.addEventListener("click", (e) =>{
+        textEl.contentEditable = 'True'
+        textEl.focus()
     })
 
     tasks.appendChild(task)
